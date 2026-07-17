f6dcfd142aba6c5c15573a82917d0ec45ddf9d4f
starbake05 <chenlee1402@gmail.com>
2026-07-17 18:36:03 +0800

    Update products.js

--- products.js
+++ products.js
@@ -1,1 +1,42 @@
-
+const products = [
+  {
+    id: 1,
+    category: "Roti Satuan",
+    name: "Roti Coklat",
+    price: 10000,
+    image: "images/roti-coklat.jpg",
+    available: true
+  },
+  {
+    id: 2,
+    category: "Roti Satuan",
+    name: "Roti Keju",
+    price: 12000,
+    image: "images/roti-keju.jpg",
+    available: true
+  },
+  {
+    id: 3,
+    category: "Pizza",
+    name: "Pizza Beef",
+    price: 85000,
+    image: "images/pizza-beef.jpg",
+    available: true
+  },
+  {
+    id: 4,
+    category: "Burger",
+    name: "Cheese Burger",
+    price: 35000,
+    image: "images/burger-cheese.jpg",
+    available: true
+  },
+  {
+    id: 5,
+    category: "Coffee",
+    name: "Cappuccino",
+    price: 25000,
+    image: "images/cappuccino.jpg",
+    available: true
+  }
+];
