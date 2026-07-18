f954755d569ad168d7f4e18a70739d8cb61af977
starbake05 <chenlee1402@gmail.com>
2026-07-18 16:21:59 +0800

    Add image preview functionality for product upload

--- admin.js
+++ admin.js
@@ -71,3 +71,26 @@ document.addEventListener("click", function(event){
     }
 
 });
+const imageInput = document.getElementById("productImage");
+
+const preview = document.getElementById("previewImage");
+
+imageInput.addEventListener("change",function(){
+
+const file = this.files[0];
+
+if(file){
+
+const reader = new FileReader();
+
+reader.onload=function(e){
+
+preview.src=e.target.result;
+
+}
+
+reader.readAsDataURL(file);
+
+}
+
+});
