28daf9ec1dd3e20551fc93c3d4c9e3a5b50367a1
starbake05 <chenlee1402@gmail.com>
2026-07-18 15:12:30 +0800

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,19 @@
+const modal = document.getElementById("productModal");
 
+const addButton = document.getElementById("addProduct");
+
+const closeButton = document.getElementById("closeModal");
+
+addButton.onclick = function () {
+    modal.style.display = "flex";
+};
+
+closeButton.onclick = function () {
+    modal.style.display = "none";
+};
+
+window.onclick = function(event){
+    if(event.target === modal){
+        modal.style.display = "none";
+    }
+};
