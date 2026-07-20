7b749181a03947257e3707203b295b355f550d9c
starbake05 <chenlee1402@gmail.com>
2026-07-20 13:30:48 +0800

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,1 +1,29 @@
+// ===============================
+// STARBAKE STORAGE V1.1
+// ===============================
 
+// Nama penyimpanan di browser
+const STORAGE_KEY = "starbake_products";
+
+// Ambil semua produk
+function getProducts() {
+
+    const data = localStorage.getItem(STORAGE_KEY);
+
+    if (!data) {
+        return [];
+    }
+
+    return JSON.parse(data);
+
+}
+
+// Simpan semua produk
+function saveProducts(products) {
+
+    localStorage.setItem(
+        STORAGE_KEY,
+        JSON.stringify(products)
+    );
+
+}
