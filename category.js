9de73f6f5aa71cfea32ebe49ef082c81241bdc51
starbake05 <chenlee1402@gmail.com>
2026-07-20 14:16:57 +0800

    Update category.js

--- category.js
+++ category.js
@@ -1,1 +1,40 @@
+// ======================================
+// STARBAKE CATEGORY MODULE V1.0
+// ======================================
 
+const categoryModal =
+document.getElementById("categoryModal");
+
+const addCategory =
+document.getElementById("addCategory");
+
+const closeCategory =
+document.getElementById("closeCategory");
+
+
+// Buka Modal
+addCategory.onclick = function(){
+
+    categoryModal.style.display = "flex";
+
+};
+
+
+// Tutup Modal
+closeCategory.onclick = function(){
+
+    categoryModal.style.display = "none";
+
+};
+
+
+// Klik area gelap
+window.onclick = function(event){
+
+    if(event.target == categoryModal){
+
+        categoryModal.style.display = "none";
+
+    }
+
+};
